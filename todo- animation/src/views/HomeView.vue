3706b01519a1bca15f64d0133860b38b20cef85d
unknown <bgsdewoo20@gmail.com>
2024-06-10 11:02:05 +0700

    Instal vue dan Mengenal Transition Component Dan Menggunakannya

---
+++ todo- animation/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const showText = ref(false)
+</script>
+
+<template>
+  <main>
+    <transition name="fade">
+      <h1 v-if="showText">Hello World!</h1>
+    </transition>
+    <button @click="showText = !showText">Toggle</button>
+  </main>
+</template>
+
+
+<style scoped>
+.fade-enter-from {
+opacity: 0;
+
+}
+
+.fade-enter-to {
+opacity: 1;  
+}
+
+.fade-enter-active {
+  transition: all 1s ease;
+}
+
+
+</style>
