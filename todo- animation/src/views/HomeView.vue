2d1f754c0a831300cb249101e5993ddbfb3a0674
unknown <bgsdewoo20@gmail.com>
2024-06-10 21:21:29 +0700

    Membuat Fungsi Create Read Delete Data Task

--- todo- animation/src/views/HomeView.vue
+++ todo- animation/src/views/HomeView.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { ref } from 'vue';
 
+const tasks = ref(['Learn HTML','Learn CSS','Learn JavaScript'])
+const newTask = ref('')
 
+function addTask () {
+  if(newTask.value) {
+    tasks.value.unshift(newTask.value)
+    newTask.value =''
+  }
+}
+
+function removeTask(task) {
+  tasks.value = tasks.value.filter((t) => t !== task)
+}
 </script>
 
 <template>
   <main>
     <div class="container">
-      <input type="text" autofocus/>
-      <div class="card-list">Task 1</div>
-      <div class="card-list">Task 2</div>
-      <div class="card-list">Task 3</div>
+      <input type="text" v-model="newTask" @keyup.enter="addTask()" autofocus />
+      <div class="card-list" v-for="(task,index) in tasks" :key="index" @click="removeTask(task)">{{ task }}</div>
     </div>
   </main>
 </template>
