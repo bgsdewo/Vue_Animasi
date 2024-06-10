cf0d120d701cd946ee1a9df6344cc55a1fbba123
unknown <bgsdewoo20@gmail.com>
2024-06-10 12:14:39 +0700

    Menggunakan Kondisi Dalam Komponen Transition

--- todo- animation/src/views/HomeView.vue
+++ todo- animation/src/views/HomeView.vue
@@ -1,28 +1,41 @@
 <script setup>
 import { ref } from 'vue';
 
-
 const showText = ref(false)
 </script>
 
 <template>
   <main>
-    <transition name="fade">
-      <h1 v-if="showText">Hello World!</h1>
-    </transition>
-    <button @click="showText = !showText">Toggle</button>
+    <div class="container">
+      <transition name="fade">
+        <h1 v-if="showText">Hello World</h1>
+        <h1 v-else>GoodBye</h1>
+      </transition>
+      <button @click="showText = !showText">Toggle</button>
+    </div>
   </main>
 </template>
 
-
 <style scoped>
-.fade-enter-from {
-opacity: 0;
 
+.container {
+  position: relative;
+}
+
+h1 {
+  position: absolute;
+}
+
+button {
+  margin-top: 100px;
+}
+
+.fade-enter-from {
+  opacity: 0;
 }
 
 .fade-enter-to {
-opacity: 1;  
+  opacity: 1;  
 }
 
 .fade-enter-active {
@@ -33,11 +46,11 @@ opacity: 1;
   opacity: 1;  
 }
 
-.fade-leave-to{
+.fade-leave-to {
   opacity: 0;
 }
 
-.fade-leave-active{
+.fade-leave-active {
   transition: all 1s ease;
 }
 </style>
